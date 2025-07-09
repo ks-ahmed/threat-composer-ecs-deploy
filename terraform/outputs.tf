ef52c0f6b10b48f2a310b4aa728641c76153c6a8
ks-ahmed <ka-sharif@outlook.com>
2025-07-09 21:44:00 +0100

    update tera

---
+++ terraform/outputs.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "domain" {
+  description = "Domain name for ACM certificate (e.g., tm.labs.example.com)"
+  type        = string
+}
+
+variable "zone_id" {
+  description = "Route53 Hosted Zone ID for the domain"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "ECS cluster name"
+  type        = string
+  default     = "todo-cluster"
+}
+
+variable "task_family" {
+  description = "ECS Task family name"
+  type        = string
+  default     = "todo-task"
+}
+
+variable "image_url" {
+  description = "Docker image URL for ECS task"
+  type        = string
+}
+
+variable "desired_count" {
+  description = "Number of ECS tasks to run"
+  type        = number
+  default     = 1
+}
