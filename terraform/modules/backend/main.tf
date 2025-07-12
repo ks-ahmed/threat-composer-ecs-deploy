c6cdb858aca4c088923da52c80fd955ee2c22c06
ks-ahmed <ka-sharif@outlook.com>
2025-07-12 16:57:14 +0100

    update backend

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -1,24 +1,31 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
 
-  versioning {
-    enabled = true
+  lifecycle {
+    prevent_destroy = true
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
- # lifecycle {
- #   prevent_destroy = true
- # }
 
   tags = var.tags
 }
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.dynamodb_table_name
@@ -29,6 +36,11 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
 
   tags = var.tags
 }
