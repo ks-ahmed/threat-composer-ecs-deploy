8a97417ed7cbcdd3007d4effb5689ed821ceb8c2
ks-ahmed <ka-sharif@outlook.com>
2025-07-20 13:39:42 +0100

    update alb/main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -40,8 +40,8 @@ resource "aws_lb_target_group" "ecs_tg" {
   health_check {
     path                = "/health"
     matcher             = "200"
-    interval            = 30
-    timeout             = 5
+    interval            = 60
+    timeout             = 10
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
